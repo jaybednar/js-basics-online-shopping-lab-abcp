c8c0acf7135c952f0a00aa69ed67bec868871d6e
Jay Bednar <ironviralmedia@gmail.com>
2018-01-21 02:58:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,13 +56,18 @@ function total() {
 
 
 function removeFromCart(item) {
-  for (var i = 0; i < cart.length; i++) {
-    if (!cart[i].hasOwnProperty(item)) {
-      console.log("That item is not in your cart.")
-      return cart;
+  var cart = getCart();
+  if (cart.length !== 0) {
+    for (var i = 0; i< cart.length; i++) {
+			if (cart[i].hasOwnProperty(item)) {
+			  cart.splice(i,1)
+			} 
     }
-  }
-} 
+  } else {
+			  console.log("That item is not in your cart.");
+			  return cart;
+			}
+}
 
 
 function placeOrder(cardNumber) {
