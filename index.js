77b5be4149b072f07d96f0c7847f2c5689b53f18
Jay Bednar <ironviralmedia@gmail.com>
2018-01-21 02:55:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function removeFromCart(item) {
   var cart = getCart();
   if (cart.length !== 0) {
     for (var i = 0; i< cart.length; i++) {
-			if (cart[i].hasOwnProperty(item) === true) {
+			if (cart[i].hasOwnProperty(item)) {
 			  cart.splice(i,1)
 			} 
     }
